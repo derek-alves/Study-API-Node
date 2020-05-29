3724a99fd4055e8edab17e9bec726a120ffd2279
derek-alves <derek.alves@fatec.sp.gov.br>
2020-05-29 11:04:15 -0300

    exercicio 1

--- 01.js
+++ 01.js
@@ -5,6 +5,29 @@ server.use(express.json());
 
 const users = ['claudio','derek','pedro','juca'];
 
+server.use((req,res,next) =>{
+    console.time('request');
+    console.log(`metodo:${req.method} || URL: ${req.url}`);
+
+    next();
+    console.timeEnd('request');
+})
+
+function checkUserExist(req,res,next){
+  if(!req.body.nome){
+    return res.status(400).json({error:"Name is required"});
+  }
+  return next();
+}
+
+function checkIdExist(req,res,next){
+  const user = users[req.params.id]
+  if(!users){
+    return res.status(400).json({error:"User does not exists!"});
+  }
+    req.user = user;
+    return next();
+}
 server.get('/user',(req,res) => {
   const nome = req.query.nome;
   return res.json({"message":`${nome}`});
@@ -14,25 +37,25 @@ server.get('/users', (req,res)=>{
   return res.json(users);
 })
 
-server.get('/users/:id',(req, res)=>{
-  const { id } = req.params;
-  return res.json({'message': users[id]})
+server.get('/users/:id',checkIdExist,(req, res)=>{
+  return res.json(req.user);
 })
 
-server.post('/users',(req,res) =>{
+server.post('/users',checkUserExist,(req,res) =>{
   const {nome} = req.body;
   users.push(nome);
+  
   return res.json(users);
 })
 
-server.put('/users/:id',(req , res)=>{
+server.put('/users/:id',checkUserExist,checkIdExist,(req , res)=>{
   const{id} = req.params;
   const {nome}= req.body;
   users[id] = nome;
   return res.json(users)
 })
 
-server.delete('/users/:id',(req,res)=>{
+server.delete('/users/:id',checkIdExist,(req,res)=>{
   const{id}= req.params;
   users.splice(id,1);
   return res.send();
